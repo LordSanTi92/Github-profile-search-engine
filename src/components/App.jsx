de68a26a6a1f66e449dafe90274b2121914a45ef
Jakub Zebrowski <j.zebrowski92@gmail.com>
2017-07-15 22:22:49 +0200

    Search engine added, app works fine with bootstrap

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import Profile from './github/Profile.jsx'
+import Search from './github/Search.jsx'
 class App extends Component{
   constructor(props){
     super(props);
@@ -8,12 +9,63 @@ class App extends Component{
       userName: 'lordsanti92',
       userData: [],
       userRepos: [],
-      perPage: 5
+      perPage: 10
     }
+  }
+  getUserData(){
+    $.ajax({
+      url: 'https://api.github.com/users/'+this.state.userName+'?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({
+          userData: data
+        })
+        console.log(data)
+      }.bind(this),
+      error: function(xhr, status, err){
+        this.setState({
+          username:null
+        })
+        alert(err)
+      }.bind(this)
+    })
+  }
+  getUserRepos(){
+    $.ajax({
+      url: 'https://api.github.com/users/'+this.state.userName+'/repos?per_page='+this.state.perPage+'&client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret+'&sort=created',
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({
+          userRepos: data
+        })
+        console.log(data)
+      }.bind(this),
+      error: function(xhr, status, err){
+        this.setState({
+          username:null
+        })
+        alert(err)
+      }.bind(this)
+    })
+  }
+  handleFormSubmit(username){
+    this.setState({
+      userName: username
+    },function() {
+      this.getUserData();
+      this.getUserRepos();
+    })
+  }
+  componentDidMount(){
+    this.getUserData();
+    this.getUserRepos();
   }
   render(){
     return <div>
-              {this.props.clientId}
+              <Search onFormSubmit = {this.handleFormSubmit.bind(this)} />
+              <Profile {...this.state}/>
             </div>
   }
 }
